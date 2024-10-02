2799bb7df070f6d1fc6fa95ac35fa48fdcea89ad
Johanne <joha786s@stud.kea.dk>
2024-10-02 17:16:59 +0200

    added search input field

--- the_tuesday_club/src/components/NavBar.tsx
+++ the_tuesday_club/src/components/NavBar.tsx
@@ -1,12 +1,13 @@
 import { HStack} from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
-
+import SearchBar from "./searchBar";
 
 
 
 const NavBar =() => {return(
-    <HStack justifyContent={"space-between"}>
+    <HStack justifyContent={"space-between"}> 
        <ColorModeSwitch />
+       <SearchBar/>
     </HStack>
 )};
 
