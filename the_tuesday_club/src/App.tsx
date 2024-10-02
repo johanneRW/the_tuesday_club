182da913f4294ae266a646de904b626f490f193a
Johanne <joha786s@stud.kea.dk>
2024-10-02 14:05:38 +0200

    set up react and chakra

---
+++ the_tuesday_club/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Divider } from '@chakra-ui/react';
+
+
+function App() {
+  return(
+    <div></div>
+  )
+}
+ 
+
+
+export default App;
