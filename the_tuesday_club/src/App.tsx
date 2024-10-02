e0d65ca7cb6c8541f2573fc8424d799a4ab742ce
Johanne <joha786s@stud.kea.dk>
2024-10-02 15:52:29 +0200

    Grid template from chakra

--- the_tuesday_club/src/App.tsx
+++ the_tuesday_club/src/App.tsx
@@ -1,11 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Divider } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 
 function App() {
   return(
-    <div></div>
+    <Grid
+  templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+  gridTemplateRows={'50px 1fr 30px'}
+  gridTemplateColumns={'150px 1fr'}
+  h='200px'
+  gap='1'
+  color='blackAlpha.700'
+  fontWeight='bold'
+>
+  <GridItem area={'header'}>
+  
+  </GridItem>
+  <GridItem  area={'nav'}>
+   
+  </GridItem>
+  <GridItem  area={'main'}>
+   
+  </GridItem>
+  <GridItem  area={'footer'}>
+   
+  </GridItem>
+</Grid>
   )
 }
  
