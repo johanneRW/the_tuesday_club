f14df1a124173dfecdb5ce9a47c69f18f34ae62b
Johanne <joha786s@stud.kea.dk>
2024-10-06 15:17:57 +0200

    dist + routes

--- the_tuesday_club/vite.config.ts
+++ the_tuesday_club/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   base: "https://the-tuesday-club-f3a8.vercel.app"
 })
