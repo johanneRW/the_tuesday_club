3866c3a815cb21333766a8e703ff591eecd9a862
Johanne <joha786s@stud.kea.dk>
2024-10-06 15:10:22 +0200

    remove out dist

--- the_tuesday_club/vite.config.ts
+++ the_tuesday_club/vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
   base: "https://the-tuesday-club-f3a8.vercel.app"
 })
