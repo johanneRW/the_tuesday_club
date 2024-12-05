daf65fb0ba442c8cde177bb5d75be57c4ec899f5
Johanne <joha786s@stud.kea.dk>
2024-12-05 12:15:30 +0100

    moved toast into hook for reusability

---
+++ the_tuesday_club/src/hooks/reuseableHooks/UseToastHandler.ts
@@ -0,0 +1,33 @@
+import { useToast } from "@chakra-ui/react";
+
+type ToastOptions = {
+  title: string;
+  description?: string;
+  status: "info" | "warning" | "success" | "error";
+  duration?: number;
+  isClosable?: boolean;
+};
+
+const useToastHandler = () => {
+  const toast = useToast();
+
+  const showToast = ({
+    title,
+    description = "",
+    status,
+    duration = 3000,
+    isClosable = true,
+  }: ToastOptions) => {
+    toast({
+      title,
+      description,
+      status,
+      duration,
+      isClosable,
+    });
+  };
+
+  return { showToast };
+};
+
+export default useToastHandler;
